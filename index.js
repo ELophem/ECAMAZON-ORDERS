7227d53d670a841e2753922b4f1de77130a05493
ELophem <18072@ecam.be>
2023-12-12 00:45:35 +0100

    Added get and delete routes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
+const orderRoutes = express.Router(); // Create a router for order-related routes
 
 // Middleware
 app.use(bodyParser.json());
@@ -19,7 +20,7 @@ const pool = mysql.createPool({
 });
 
 // Route to handle storing orders
-app.post('/storeOrder', (req, res) => {
+orderRoutes.post('/storeOrder', (req, res) => {
   const { orderId, articles, amount, clientName } = req.body;
 
   // Assuming your database has a table named 'orders'
@@ -36,6 +37,48 @@ app.post('/storeOrder', (req, res) => {
     }
   );
 });
+
+// Route to handle deleting an order by order_id
+orderRoutes.delete('/deleteOrder/:orderId', (req, res) => {
+  const orderId = req.params.orderId;
+
+  // Assuming your database has a table named 'orders'
+  pool.query(
+    `DELETE FROM orders WHERE order_id = ?`,
+    [orderId],
+    (err, results) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Failed to delete order' });
+      } else {
+        if (results.affectedRows === 0) {
+          res.status(404).json({ message: 'Order not found' });
+        } else {
+          res.status(200).json({ message: 'Order deleted successfully' });
+        }
+      }
+    }
+  );
+});
+
+// Route to fetch all orders
+orderRoutes.get('/orders', (req, res) => {
+  // Assuming your database has a table named 'orders'
+  pool.query(
+    `SELECT * FROM orders`,
+    (err, results) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Failed to fetch orders' });
+      } else {
+        res.status(200).json(results);
+      }
+    }
+  );
+});
+
+// Mount the order routes at /api/orders
+app.use('/api/orders', orderRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 4000;
